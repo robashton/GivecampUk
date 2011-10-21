b6ff655531bc844af991027074e16d476790c9f0
Rob Ashton <robashton@codeofrob.com>
2011-10-21 22:41:55 +0200

    express...

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
+// Telephone support: 07939 012 139
+
 http = require('http'),  
 path = require('path'),
 fs = require('fs');
-paperboy = require('paperboy');
 
-ROOT = path.dirname(__filename) + "/site";
+var express = require('express');
+var app = express.createServer();
 
-server = http.createServer(function(req, res){ 
-	  paperboy
-	  .deliver(ROOT, req, res)
-	  .addHeader('Cache-Control', 'no-cache')
-	  .otherwise(function(){
-    
-        if(req.url.indexOf("/services") == 0) {
-			   	  res.writeHead(200, "Content-Type: text/plain");
-			      res.write("Services will be found here");
-			      res.end();	
-		    } else {
-			    res.writeHead(404, "Content-Type: text/plain");
-			    res.write("Not found and all that");
-			    res.end();
-        }
-	  });
+app.get('/', express.static(__dirname + '/site'));
 
+app.get('/service', function(req, res){
+  res.send('hello world');
 });
-server.listen(8081);
+
+app.listen(8081);
 
 console.log("Listening on port 8081");
 
