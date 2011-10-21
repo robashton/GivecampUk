d93bd6093a8bd91a7a87f0aa2baecaac787c72e0
BenHall <Ben@Benhall.me.uk>
2011-10-21 20:52:33 +0100

    CouchDB hooked up

--- server.js
+++ server.js
@@ -14,7 +14,23 @@ server = http.createServer(function(req, res){
         if(req.url.indexOf("/services") == 0) {
 			   	  res.writeHead(200, "Content-Type: text/plain");
 			      res.write("Services will be found here");
-			      res.end();	
+			      
+			      var CouchClient = require('couch-client');
+            var db = CouchClient("http://localhost:5984/youngmindsdb");
+			      
+            db.save({_id: "creationix", name: "Tim Caswell", age: 28}, function ( err, doc) {
+              // You know know if there was an error and have an updated version
+              // of the document (with `_id` and `_rev`).
+            });
+
+
+            db.get("creationix", function (err, doc) {
+              // Now you have the document or error if there was trouble
+              res.write(JSON.stringify(doc))
+              res.end();	
+            });
+			      
+			     
 		    } else {
 			    res.writeHead(404, "Content-Type: text/plain");
 			    res.write("Not found and all that");
