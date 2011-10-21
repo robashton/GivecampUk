0401584f3473df16ef77c233b7b777e2fd127916
Rob Ashton <robashton@codeofrob.com>
2011-10-21 23:17:44 +0200

    Added authentication and sessions

--- server.js
+++ server.js
@@ -3,17 +3,36 @@
 http = require('http'),  
 path = require('path'),
 fs = require('fs');
+Cookies = require('cookies');
 
 var express = require('express');
 var app = express.createServer();
 
+function validateUser(req, res) {
+  var cookies = new Cookies( req, res );
+  var username = cookies.get( "username" );
+
+  if(!username) {
+   res.json({ success: false}, {}, 401); 
+   return false;
+  }
+  return true;
+};
+
 app.get('/', express.static(__dirname + '/site'));
 
-app.get('/service', function(req, res){
-  res.send('hello world');
+app.get('/login', function(req, res){
+   var cookies = new Cookies( req, res );
+   cookies.set( "username", "Emma", { httpOnly: false } );
+   res.json({ success: true}, {}, 200);  
 });
 
-app.listen(8081);
+app.get('/currentuser', function(req, res) {
+  if(!validateUser(req,res)) return;
+  res.json({ username: 'Emma'}, {}, 200);
+});
 
-console.log("Listening on port 8081");
+app.listen(8080);
+
+console.log("Listening on port 8083");
 
