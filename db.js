e33c0af5d61f072ae315ebd0413c51a6f1ede29a
Rob Ashton <robashton@codeofrob.com>
2011-10-22 13:05:15 +0200

    added promote/demote user

--- db.js
+++ db.js
@@ -39,7 +39,7 @@ exports.create_user = function () {
   encryption.hash("password", function(hash){
     db.save({questionId: req.param('qId')})
     
-    db.save({_id: "test", name: "Tim Caswell", age: 28, email: "Tim.Caswell@gmail.com", password: hash, type: "user"}, function ( err, doc) {
+    db.save({_id: "test", name: "Tim Caswell", age: 28, email: "Tim.Caswell@gmail.com", password: hash, type: "user", isElevated: false}, function ( err, doc) {
       // You know know if there was an error and have an updated version
       // of the document (with `_id` and `_rev`).
     });
