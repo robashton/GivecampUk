36422d6c20f03df8371bddbce51172458de96139
Rob Ashton <robashton@codeofrob.com>
2011-10-22 16:26:40 +0200

    Hooked up answers on the question page

--- site/views/ym.viewquestion.js
+++ site/views/ym.viewquestion.js
@@ -2,12 +2,14 @@ var viewQuestionModel = {
       id: ko.observable(''),
       title: ko.observable(''),
       description: ko.observable(''),
+      answers: ko.observableArray([]),
 
       init: function() {
         $.getJSON('/question/' + viewQuestionModel.id(), function(data) {   
             if(data.error) console.log(data.error);         
             viewQuestionModel.title(data.question.title);
             viewQuestionModel.description(data.question.description);
+            viewQuestionModel.answers(data.answers.rows);
         });
       },   
 };
