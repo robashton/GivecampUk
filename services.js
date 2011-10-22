aaf348196ba2086c97fed112819d4f3bff34ba16
Rob Ashton <robashton@codeofrob.com>
2011-10-22 11:13:20 +0200

    Fixed validation and tested service

--- services.js
+++ services.js
@@ -58,7 +58,8 @@ exports.init = function(app) {
   }); 
 
   app.post('/createquestion', function(req, res) {
-      if(!expect({
+    
+      if(!expect(req, res, {
         title: "There must be a title",
         description: "There must be a description",
         tag: "There must be a selected tag"
@@ -73,16 +74,16 @@ exports.init = function(app) {
         user:userid, 
         date:new Date(),
         deleted:0,
-        title: req.params.title, 
-        description: req.params.description,
-        tag: req.params.tag
+        title: req.body.title, 
+        description: req.body.description,
+        tag: req.body.tag
         }, 
        function ( err, doc) {
           res.json({
             err: err,
             doc: doc
           });
-       });      
+       });    
   }); 
 
   app.get('/service', security.validateUser, function(req, res){
@@ -93,13 +94,12 @@ exports.init = function(app) {
 
   expect = function(req, res, keys) {
     for(i in keys) {
-      var value = req.params[i];
+      var value = req.body[i];
       if(!value) { 
         res.json({ error: keys[i]});
         return false;
       }
-      return true;
-
     }
+    return true;
   }
 };
