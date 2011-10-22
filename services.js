445bd3f51fb0f2d1c889c34f0da358009d6e0050
BenHall <Ben@Benhall.me.uk>
2011-10-22 09:23:07 +0100

    Not sure

--- services.js
+++ services.js
@@ -5,12 +5,30 @@ var security = require('./security');
 exports.init = function(app) {
 
   app.configure(function(){
+    app.use(express.bodyParser());
     app.use(express.static(__dirname + '/site'));
-      app.use(express.bodyParser());
   });
 
   app.post('/login', function(req, res){
-    security.signInUser(req, res, req.body.username, req.body.password);
+    console.log(req.body.loginUser)
+    security.signInUser(req, res, "Tim.Caswell@gmail.com", req.body.loginPass, function(result) {
+      console.log(result)
+      if(!result){
+          res.json({ success: false}, {}, 401); 
+        }
+        else
+        { 
+          var cookies = new Cookies( req, res );
+          cookies.set( "username", email, { httpOnly: false } );
+          res.json({success: true},{},200);
+        }
+    });
+    
+  });
+  
+  app.get('/register', function(req, res){
+    db.create_user()
+    res.json({ success: true}, {}, 200);  
   });
 
   app.get('/logout', function(req, res){
@@ -27,9 +45,5 @@ exports.init = function(app) {
         res.send('hello world: ' + doc);
       })
   });
-
-
-
-  
 
 };
