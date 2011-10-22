72c9e311bf25a62a6329937c12fda8d461bff036
Scott Cowan <info@scottcowan.org>
2011-10-22 18:03:52 +0200

    fixed password registration and lowercased email

--- db.js
+++ db.js
@@ -22,7 +22,7 @@ exports.save_answer = function(question_id, answer_text, callback) {
 }
 
 exports.get_user = function (email, callback) {
-  db.view('/youngmindsdb/_design/users/_view/by_email', {key: email}, function(err, doc) {
+  db.view('/youngmindsdb/_design/users/_view/by_email', {key: email.toLowerCase()}, function(err, doc) {
       // Now you have the document(s) or error if there was trouble
       callback(err,doc)
   });
@@ -54,15 +54,15 @@ exports.get_questions_by_rank = function(callback){
 
 exports.create_session = function (id,email, callback) {
     var guid =  utils.generateGuid();
-    db.save({session: guid, email: email, type: "session"}, function ( err, doc) {
+    db.save({session: guid, email: email.toLowerCase(), type: "session"}, function ( err, doc) {
       callback(guid);
     });
   }
 
 exports.create_user = function (email,name, password) {
-  encryption.hash("password", function(hash){
+  encryption.hash(password, function(hash){
     
-    db.save({email: email, name:name, password: hash, type: "user", isElevated: false}, function ( err, doc) {
+    db.save({email: email.toLowerCase(), name:name, password: hash, type: "user", isElevated: false}, function ( err, doc) {
       // You know know if there was an error and have an updated version
       // of the document (with `_id` and `_rev`).
     });
