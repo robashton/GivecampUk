ea3ec61f53737eac814d91c881889193c4ca3471
Rob Ashton <robashton@codeofrob.com>
2011-10-22 18:38:44 +0200

    Made the questions list work again

--- site/views/ym.questions.js
+++ site/views/ym.questions.js
@@ -8,7 +8,7 @@ questionsModel.init = function() {
     $.get('get_questions_by_tag/').success(function(data) {
 
         if(!data.error) {
-            questionsModel.questions(data.rows);
+            questionsModel.questions(data.doc.rows);
         }
     });
 
