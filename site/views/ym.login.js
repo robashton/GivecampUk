0e0a1518efb8515407f11ad720902a288fdc924a
Dan Kendall <dan@danstuken.com>
2011-10-23 10:06:00 +0100

    Validation on registration form

--- site/views/ym.login.js
+++ site/views/ym.login.js
@@ -4,10 +4,12 @@ var loginModel = {
         email: ko.observable(''),
         displayName: ko.observable(''),
         password: ko.observable(''),
-        registrationError: ko.observable(''),
+        validationMessage: ko.observable(''),
 
         submit: function(event){
             var form = loginModel.registerForm;
+            if(!form.preSubmitValidate(form))
+               return;
             $.post('register', {email: form.email() ,name: form.displayName(), password: form.password() }).success(function(data){
                if(data.success) {
                   //window.location = 'index.html';
@@ -16,15 +18,32 @@ var loginModel = {
                   }, 1500);
                   window.location = 'app.html';
               }else{
-                  loginModel.registerForm.registrationError(data.error);
+                  loginModel.registerForm.validationMessage(data.error);
               }
             });
+        },
+
+        preSubmitValidate: function(form){
+           var message = '';
+           if(form.email().length < 1)
+              message = message + 'Please provide an email address';
+           if(form.displayName().length < 1)
+              message = message + '\nPlease provide a display name';
+           if(form.password().length < 1)
+              message = message + '\nPlease provide a password';
+
+           if(message.length > 0){
+              form.validationMessage(message);
+              return false;
+           }
+           return true;
         }
     },
 
     loginForm: {
         email: ko.observable(''),
         password: ko.observable(''),
+        validationMessage: ko.observable(''),
 
         submit: function(event){
             var form = loginModel.loginForm;
@@ -38,11 +57,19 @@ var loginModel = {
               window.location = 'app.html';
             }
         });
-    }
+    },
+
+    //we are overloading 'validation' here - covers auth/auth errors too
 };
 
-$(function(){
    loginModel.loginForm.valid = ko.dependentObservable(function(){
-   return loginModel.loginForm.email().length > 0 && loginModel.loginForm.password().length > 0;
+      return loginModel.loginForm.email().length > 0 && loginModel.loginForm.password().length > 0;
    }, loginModel);
-});
+
+   loginModel.registerForm.hasValidationIssue = ko.dependentObservable(function() {
+      return loginModel.registerForm.validationMessage().length > 0;
+   }, loginModel);
+
+   loginModel.loginForm.hasValidationIssue = ko.dependentObservable(function() {
+      return loginModel.loginForm.validationMessage().length > 0;
+   }, loginModel);
